0ed13ceb66aabe1158af01a6dd8b32ec99519288
sakuramoti0125 <stsu23hiro@icloud.com>
2024-10-31 14:17:25 +0900

    aho

--- test.bash
+++ test.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-ng (){
-        echo ${1}行目が違うよ
+ng () {
+	echo ${1}行目が違うよ
 	res=1
 }
 
@@ -10,5 +10,13 @@ res=0
 out=$(seq 5 | ./plus)
 [ "${out}" = 15 ] || ng "$LINENO"
 
-[ "${res}" = 0 ] && echo OK
-exit $res
+out=$(echo あ| ./plus)
+[ "$?" = 1 ] || ng "$LINENO"
+[ "${out}" = "" ] || ng "$LINENO"
+
+out=$(echo | ./plus)
+[ "$?" = 1 ] || ng "$LINENO"
+[ "${out}" = "" ] || ng "$LINENO"
+
+[ "$res" = 0 ] && echo OK
+exit "$res"
